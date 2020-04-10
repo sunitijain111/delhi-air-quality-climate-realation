c122780a712ea573cc579b80372b8525b0db431a
Suniti Jain <57194884+sunitijain111@users.noreply.github.com>
2020-04-10 14:29:36 +0530

    Update delhi-air-pollution.py

--- delhi-air-pollution.py
+++ delhi-air-pollution.py
@@ -1,7 +1,9 @@
 import pandas as pd
-
+import numpy as np
 df1= pd.read_excel('delhi air 1996 to 2015.xls')
 df2= pd.read_excel('delhi weather since 1997.xlsx') 
 
 df3= pd.merge(df1, df2, how='inner', on=['month','year'])
-df3.fillna(df3.mean())
+df3.fillna(df3.mean(), inplace= True)
+
+df3=df3.drop('PM 2.5', axis=1)
